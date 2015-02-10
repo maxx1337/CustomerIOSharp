3ef05f2c0c4361a61a4f9615ea27cfa119ddd7c9
Maxim <maxx1337@gmail.com>
2015-02-10 15:57:00 +0100

    it is now possible to set customerid on each action

--- CustomerIOSharp/CustomerIO.cs
+++ CustomerIOSharp/CustomerIO.cs
@@ -19,6 +19,11 @@ namespace CustomerIOSharp
 
         private readonly RestClient _client;
 
+        public CustomerIo(string siteId, string apiKey)
+            : this(siteId, apiKey, null)
+        {
+        }
+
         public CustomerIo(string siteId, string apiKey, ICustomerFactory customerFactory)
         {
             _siteId = siteId;
@@ -29,6 +34,14 @@ namespace CustomerIOSharp
                 {
                     Authenticator = new HttpBasicAuthenticator(_siteId, _apiKey)
                 };
+        }
+
+        private void CheckCustomerFactory()
+        {
+            if (_customerFactory == null)
+            {
+                throw new ArgumentNullException("customerFactory", "set customerFactory param in the ctor");
+            }
         }
 
         private async Task CallMethodAsync(string method, string customerId, Method httpMethod, object data)
@@ -44,7 +57,7 @@ namespace CustomerIOSharp
             };
             request.AddUrlSegment(@"customer_id", customerId);
             request.AddBody(data);
-            
+
             var response = await _client.ExecuteAsync(request);
             if (response.StatusCode != HttpStatusCode.OK)
             {
@@ -52,21 +65,49 @@ namespace CustomerIOSharp
             }
         }
 
+        /// <summary>
+        /// Update the customer. Can only be used if customerFactory was defined
+        /// </summary>
+        /// <returns></returns>
         public async Task IdentifyAsync()
         {
+            CheckCustomerFactory();
             var id = _customerFactory.GetCustomerId();
-
-            await CallMethodAsync(MethodCustomer, id, Method.PUT, _customerFactory.GetCustomerDetails());
-        }
-
-        public async Task DeleteCustomerAsync()
-        {
-            var id = _customerFactory.GetCustomerId();
-            await CallMethodAsync(MethodCustomer, id, Method.DELETE, null);
+            var customerDetails = _customerFactory.GetCustomerDetails();
+            await IdentifyAsync(id, customerDetails);
         }
 
         /// <summary>
-        /// Track a custom event.
+        /// Update the customer
+        /// </summary>
+        /// <returns></returns>
+        public async Task IdentifyAsync(string customerId, object customerDetails)
+        {
+            await CallMethodAsync(MethodCustomer, customerId, Method.PUT, customerDetails);
+        }
+
+        /// <summary>
+        /// Delete the customer. Can only be used if customerFactory was defined
+        /// </summary>
+        /// <returns></returns>
+        public async Task DeleteCustomerAsync()
+        {
+            CheckCustomerFactory();
+            var id = _customerFactory.GetCustomerId();
+            await DeleteCustomerAsync(id);
+        }
+
+        /// <summary>
+        /// Delete the customer.
+        /// </summary>
+        /// <returns></returns>
+        public async Task DeleteCustomerAsync(string customerId)
+        {
+            await CallMethodAsync(MethodCustomer, customerId, Method.DELETE, null);
+        }
+
+        /// <summary>
+        /// Track a custom event. Can only be used if customerFactory was defined
         /// </summary>
         /// <param name="eventName">The name of the event you want to track</param>
         /// <param name="data">Any related information you’d like to attach to this event. These attributes can be used in your triggers to control who should receive the triggered email. You can set any number of data key and values.</param>
@@ -75,8 +116,22 @@ namespace CustomerIOSharp
         /// <exception cref="CustomerIoApiException">If any code besides 200 OK is returned from the server.</exception>
         public async Task TrackEventAsync(string eventName, object data = null, DateTime? timestamp = null)
         {
+            CheckCustomerFactory();
             var id = _customerFactory.GetCustomerId();
+            await TrackEventAsync(id, eventName, data, timestamp);
+        }
 
+        /// <summary>
+        /// Track a custom event.
+        /// </summary>
+        /// <param name="customerId">The id of the customer you want to track</param>
+        /// <param name="eventName">The name of the event you want to track</param>
+        /// <param name="data">Any related information you’d like to attach to this event. These attributes can be used in your triggers to control who should receive the triggered email. You can set any number of data key and values.</param>
+        /// <param name="timestamp">Allows you to back-date the event, pass null to use current time</param>
+        /// <returns>Nothing if successful, throws if failed</returns>
+        /// <exception cref="CustomerIoApiException">If any code besides 200 OK is returned from the server.</exception>
+        public async Task TrackEventAsync(string customerId, string eventName, object data = null, DateTime? timestamp = null)
+        {
             var wrappedData = new TrackedEvent
                 {
                     Name = eventName,
@@ -84,7 +139,7 @@ namespace CustomerIOSharp
                     Timestamp = timestamp
                 };
 
-            await CallMethodAsync(MethodTrack, id, Method.POST, wrappedData);
+            await CallMethodAsync(MethodTrack, customerId, Method.POST, wrappedData);
         }
     }
 
