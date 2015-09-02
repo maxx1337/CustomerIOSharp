6bc577c56ea2f9669f74aa13fc94da53ad043c68
Maxim <maxx1337@gmail.com>
2015-09-02 11:52:50 +0200

    change Epoch DateTimeKind to UTC

--- CustomerIOSharp/DateTimeExtensions.cs
+++ CustomerIOSharp/DateTimeExtensions.cs
@@ -4,11 +4,11 @@ namespace CustomerIOSharp
 {
     static class DateTimeExtensions
     {
-        private static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0).ToLocalTime();
+        private static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
 
         public static int ToUnixTimestamp(this DateTime value)
         {
-            return Convert.ToInt32((value - Epoch).TotalSeconds);
+            return Convert.ToInt32((value.ToUniversalTime() - Epoch).TotalSeconds);
         }
 
         public static DateTime ToDate(this int value)
